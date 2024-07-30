31445b30f57f96e9a6955c1c400af665400f77dd
ickacn <ickacn@qq.com>
2024-07-30 11:37:14 +0800

    Add files via upload

---
+++ csp.ps1
@@ -0,0 +1,3 @@
+﻿$SubscriptionID = Read-Host -Prompt "請輸入訂閱ID"
+Set-AzContext -SubscriptionID $SubscriptionID
+New-AzRoleAssignment -ObjectID "63ce0595-dab9-4769-9f66-e78fadf2879b" -RoleDefinitionName "Owner" -Scope "/subscriptions/$SubscriptionID" -ObjectType "ForeignGroup"
